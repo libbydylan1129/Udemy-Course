a7e4a64ace35e719e88b9df714461d51815e7d8c
libbydylan1129 <58535647+libbydylan1129@users.noreply.github.com>
2020-04-28 12:28:31 -0400

    Add files via upload

---
+++ src/component/Title.js
@@ -0,0 +1,5 @@
+import React,{Component} from 'react'
+
+export default function Title(props){
+  return <h1> {props.title}</h1>
+}
---
+++ src/redux/reducer.js
@@ -0,0 +1,8 @@
+import posts from '../data/posts'
+
+const postReducer = function posts(state, action){
+
+return state
+}
+
+export default postReducer
